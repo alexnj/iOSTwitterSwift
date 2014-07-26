b5e21f6b76f3a52f5974009d026cd4eecb53885e
Alex Jose <alexnj@gmail.com>
2014-07-26 15:28:11 -0700

    Added a singleton mechanism based on current Swift limitations.

--- iOSTwitterSwift/TwitterClient.swift
+++ iOSTwitterSwift/TwitterClient.swift
@@ -10,4 +10,14 @@ import Foundation
 
 class TwitterClient: BDBOAuth1RequestOperationManager {
     
+    // Singleton sharedInstance implementation.
+    // Using struct here since classes don't support static variables,
+    // but structs do.
+    class var sharedInstance : TwitterClient {
+        struct Static {
+            static let instance : TwitterClient = TwitterClient()
+        }
+        return Static.instance
+    }
+    
 }
