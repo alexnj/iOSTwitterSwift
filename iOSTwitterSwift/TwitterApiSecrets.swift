f14c6e778494ebb8e2936c9bd424ae5f9074e7e0
Alex Jose <alexnj@gmail.com>
2014-07-26 15:15:26 -0700

    Added an API client and API secrets files.

---
+++ iOSTwitterSwift/TwitterApiSecrets.swift
@@ -0,0 +1,10 @@
+//
+//  TwitterApiSecrets.swift
+//  iOSTwitterSwift
+//
+//  Created by Alex on 7/26/14.
+//  Copyright (c) 2014 alexnj. All rights reserved.
+//
+
+let consumerKey:String = ""
+let consumerSecret:String = ""
---
+++ iOSTwitterSwift/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  iOSTwitterSwift
+//
+//  Created by Alex on 7/26/14.
+//  Copyright (c) 2014 alexnj. All rights reserved.
+//
+
+import Foundation
+
+class TwitterClient: BDBOAuth1RequestOperationManager {
+    
+}
